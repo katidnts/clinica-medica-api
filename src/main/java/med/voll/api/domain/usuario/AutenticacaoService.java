1eec53e9b4760ed461fc092313963d399c312b70
Kati Dnts <katidnts@gmail.com>
2026-01-14 17:18:19 -0300

    Implementa autenticação stateless com Spring Security

--- src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -1,9 +1,12 @@
 package med.voll.api.domain.usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,4 +19,6 @@ public class AutenticacaoService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByLogin(username);
     }
+
+
 }
